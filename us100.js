bf5e74f5c07d4418c9b0c338e736e056fa77fd55
Mikael Vester <pmvester@gmail.com>
2016-11-06 07:26:18 +0100

    first working versiongit add us100.js !

--- us100.js
+++ us100.js
@@ -1,17 +1,32 @@
 module.exports = function(RED) {
   "use strict";
+
   var Gpio = require('pigpio').Gpio,
     trigger = new Gpio(23, {mode: Gpio.OUTPUT}),
     echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
   var MICROSECONDS_PER_CM = 1e6/34321;
+
   trigger.digitalWrite(0);
+
+  var startTick, endTick, diff;
+  var msg = {payload: null};
+
   function Us100(config) {
     RED.nodes.createNode(this,config);
-      var node = this;
-      this.on('input', function(msg) {
-        msg.payload = msg.payload.toLowerCase();
+    var node = this;
+    echo.on('alert', function (level, tick) {
+      if (level == 1) {
+        startTick = tick;
+      } else {
+        endTick = tick;
+        diff = (endTick >> 0) - (startTick >> 0);
+        msg.payload = diff / 2 / MICROSECONDS_PER_CM;
         node.send(msg);
+      }
     });
+    setInterval(function () {
+      trigger.trigger(10, 1);
+    }, 1000);
   }
   RED.nodes.registerType("us100",Us100);
 }
