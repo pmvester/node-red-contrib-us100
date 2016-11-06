ae8d26e6c4298e55d01ad86d05aa08e7e6eaf102
Mikael Vester <pmvester@gmail.com>
2016-11-06 06:59:50 +0100

    added some files

--- us100.js
+++ us100.js
@@ -1,11 +1,17 @@
 module.exports = function(RED) {
-    function Us100(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("us100",Us100);
+  "use strict";
+  var Gpio = require('pigpio').Gpio,
+    trigger = new Gpio(23, {mode: Gpio.OUTPUT}),
+    echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+  var MICROSECONDS_PER_CM = 1e6/34321;
+  trigger.digitalWrite(0);
+  function Us100(config) {
+    RED.nodes.createNode(this,config);
+      var node = this;
+      this.on('input', function(msg) {
+        msg.payload = msg.payload.toLowerCase();
+        node.send(msg);
+    });
+  }
+  RED.nodes.registerType("us100",Us100);
 }
